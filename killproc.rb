9edafb693d6d4252239ea047dadd587144704e9b
xchewmer6666 <xchewmer6666@gmail.com>
2023-07-29 20:19:25 +0000

    23.07.29/commit02

--- killproc.rb
+++ killproc.rb
@@ -2,41 +2,48 @@
 
 require 'optparse'
 
-puts '''
-                            __   _ ____
-     ____  _________  _____/ /__(_) / /
-    / __ \/ ___/ __ \/ ___/ //_/ / / / 
-   / /_/ / /  / /_/ / /__/ ,< / / / /  
-  / .___/_/   \____/\___/_/|_/_/_/_/   
- /_/       
-                 written by xchewmer
-'''
+def banner
+  puts '''
+                              __   _ ____
+       ____  _________  _____/ /__(_) / /
+      / __ \/ ___/ __ \/ ___/ //_/ / / / 
+     / /_/ / /  / /_/ / /__/ ,< / / / /  
+    / .___/_/   \____/\___/_/|_/_/_/_/   
+   /_/       
+                   written by xchewmer
+  '''
 
-puts
-
-options = {}
-OptionParser.new do |opt|
-  opt.on('--name NAME') { |o| options[:name] = o }
-end.parse!
-
-if !options[:name]
-  puts "[!] please enter a valid value or regex"
-  exit
+  puts
 end
 
-out = `ps aux | grep #{options[:name]} > test.txt`
+def main
+  options = {}
+  OptionParser.new do |opt|
+    opt.on('--name NAME') { |o| options[:name] = o }
+  end.parse!
 
-File.open("./test.txt", "r") do |f|
-  f.each_line do |line|
-    pid = line.split(' ')[1]
-    begin
-      if !(line.include? "ruby" or line.include? "grep")
-        puts "[+] killing pid #{pid}"
-        out = `sudo kill -9 #{line.split(' ')[1]} > kill.txt`
+  if !options[:name]
+    puts "[!] please enter a valid value or regex"
+    exit
+  end
+
+  out = `ps aux | grep #{options[:name]} > test.txt`
+
+  File.open("./test.txt", "r") do |f|
+    f.each_line do |line|
+      pid = line.split(' ')[1]
+      begin
+        if !(line.include? "ruby" or line.include? "grep")
+          puts "[+] killing pid #{pid}"
+          out = `sudo kill -9 #{line.split(' ')[1]} > kill.txt`
+        end
+      rescue
+        puts "[!] error on killing #{pid}"
       end
-    rescue
-      puts "[!] error on killing #{pid}"
+      puts
     end
-    puts
   end
 end
+
+banner
+main
